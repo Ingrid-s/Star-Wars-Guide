a00fe90b9163a9bd8a37f2a568a595700354cd2f
ingridsag <starlling@hotmail.com>
2018-03-06 01:33:52 -0600

    primeros avances, obtener nombre, episodio, falta corregir el personaje

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let printBox = document.getElementById('container');
+
+$.ajax({
+     type: "GET",
+     url: "https://swapi.co/api/films/",
+ }).done (obtainData);
+ 
+
+ function obtainData (data) {
+      let moviesColection = data.results;
+      console.log(moviesColection);   
+      show(moviesColection);
+ };
+
+ function show (element) {
+      let print = " ";
+      let printPeople = ' ';
+      element.forEach(item =>{
+           let movieTitle = item.title;
+           console.log(movieTitle);
+           let people = item.characters;
+           console.log(people);
+           let cronology = item.episode_id;
+           console.log(cronology +=" episode");
+     print +=
+     `
+     <section class="card-panel teal black col s12">
+          <h6 class="yellow-text  ">${movieTitle} </h6>
+          <p class="white-text text-darken-2">${cronology}</p>
+          <p class="white-text text-darken-2">${people}</p>
+       </section>`
+
+       
+
+       printBox.innerHTML=print;
+
+
+      })
+ }
+     
+ 
